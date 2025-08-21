e71ad569430375eceb7c0255e146a2ceeef9ac14
shrinathprabhu <shrinathprabhu.123@gmail.com>
2025-08-21 13:46:23 +0530

    Update the next loading after previous step is done

--- src/injected/caEvents.ts
+++ src/injected/caEvents.ts
@@ -180,14 +180,11 @@ export function setCAEvents(ca: CA) {
         <div class="sc-bjfHbI jxtURp body12Regular" style="color: rgb(80, 210, 193) !important; text-align: center; display: block;" bis_skin_checked="1"><a href="${data.data.explorerURL}" style="text-decoration: underline; color: currentColor" target="_blank">View Intent</a></div>
         `;
         }
-        // explorerURL = data.data.explorerURL;
-        break;
-      }
-      case "INTENT_HASH_SIGNED": {
-        const intentDiv = mainDiv.querySelector("#intent-collection");
-        if (intentDiv) {
-          intentDiv.children[0].classList.add("loader");
+        const collectionDiv = mainDiv.querySelector("#intent-collection");
+        if (collectionDiv) {
+          collectionDiv.children[0].classList.add("loader");
         }
+        // explorerURL = data.data.explorerURL;
         break;
       }
       case "INTENT_COLLECTION": {
